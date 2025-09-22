33c98b7f8dad582038d9bd82d12c22991000c87c
PedjaWasTaken <pedjamaslaric@gmail.com>
2025-09-22 08:26:26 +0200

    Flight basic movement and display
    Logging client side,
    Server side real time map update

--- Kontrola Leta/Client/Client.cs
+++ Kontrola Leta/Client/Client.cs
@@ -1,15 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using Library;
+
+
 
 namespace Client
 {
     internal class Client
     {
+        public static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Client Zapocet");
+
+            
+
+            Console.Write("Unesite pocetnu X koordinatu: ");
+            int startX = int.Parse(Console.ReadLine());
+            Console.Write("Unesite pocetnu Y koordinatu: ");
+            int startY = int.Parse(Console.ReadLine());
+            int startZ = 1; // Uvek 1
+
+            Console.Write("Unesite krajnu X koordinatu: ");
+            int endX = int.Parse(Console.ReadLine());
+            Console.Write("Unesite krajnu Y koordinatu: ");
+            int endY = int.Parse(Console.ReadLine());
+
+
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverUdpEP = new IPEndPoint(IPAddress.Loopback, 1234);
+
+            try
+            {
+                // UDP slanje
+                string request = $"REQUEST;{startX};{startY};{startZ};{endX};{endY};";
+                byte[] requestBytes = Encoding.UTF8.GetBytes(request);
+                udpSocket.SendTo(requestBytes, serverUdpEP);
+
+                // UDP odgovor
+                udpSocket.ReceiveTimeout = 3000;
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] responseBytes = new byte[1024];
+                int received = udpSocket.ReceiveFrom(responseBytes, ref remoteEP);
+                string response = Encoding.UTF8.GetString(responseBytes, 0, received);
+                Console.WriteLine("UDP odgovor: " + response);
+
+                string[] parts = response.Split(';');
+                if (parts[0] == "OK" || parts[0] == "CORRECTED")
+                {
+                    // Potvrda, i povratak informacija
+                    int confirmedX = int.Parse(parts[1]);
+                    int confirmedY = int.Parse(parts[2]);
+                    int confirmedZ = int.Parse(parts[3]);
+
+                    int maxPutnika = random.Next(50, 200);
+
+                    // TCP komunikacija
+                    using (Socket tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                    {
+                        tcpSocket.Connect(serverUdpEP.Address, 2345);
+                        using (NetworkStream ns = new NetworkStream(tcpSocket))
+                        {
+
+                            //Avion je nasumicno generisan
+                            Letelica letelica = new Letelica
+                            {
+                                imeLetelice = RandomString(15),
+                                imePilota = RandomString(10),
+                                registracijaLetelice = RandomString(20),
+                                maxPutnika = maxPutnika,
+                                trenutnoPutnika = random.Next(1,maxPutnika)
+                            };
+                            Let let = new Let
+                            {
+                                letelica = letelica,
+                                axisStartX = confirmedX,
+                                axisStartY = confirmedY,
+                                axisStartZ = confirmedZ,
+                                axisEndX = endX,
+                                axisEndY = endY,
+                                
+                            };
+
+                            // Send Let object
+                            BinaryFormatter formatter = new BinaryFormatter();
+                            formatter.Serialize(ns, let);
+
+                            // 4. Receive next positions from server
+                            byte[] buffer = new byte[1024];
+                            while (true)
+                            {
+                                int bytesRead = ns.Read(buffer, 0, buffer.Length);
+                                string nextPos = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                                Console.WriteLine("Next position: " + nextPos);
+                                if (nextPos.StartsWith("ARRIVED")) break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Flight request denied or invalid response.");
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            udpSocket.Close();
+            Console.WriteLine("Client closed.");
+            Console.ReadLine();
         }
     }
 }
+    
--- Kontrola Leta/Kontrola Leta/Server.cs
+++ Kontrola Leta/Kontrola Leta/Server.cs
@@ -1,96 +1,243 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-using System.IO;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters.Binary;
-using Library;
-
-
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Kontrola_Leta
 {
     internal class Server
     {
+        
         static void Main(string[] args)
         {
-            int N = 30;
-            int M = 10;
+            Server server = new Server();
+            server.StartAsync().GetAwaiter().GetResult();
+        }
 
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint server = new IPEndPoint(IPAddress.Any, 15011);
-            serverSocket.Bind(server);
-            serverSocket.Blocking = false;
-            int maxLetova = 20;
-            serverSocket.Listen(maxLetova);
+        private const int UdpPort = 1234;
+        private const int TcpPort = 2345;
 
+        private UdpClient udpListener;
+        private TcpListener tcpListener;
 
-            byte[] buffer = new byte[4096];
-            bool kraj = false;
-            Random r = new Random();
+        // Store active flights
+        private static readonly List<Let> ActiveFlights = new List<Let>();
+        private static readonly object LockObj = new object();
 
-            List<Socket> letovi = new List<Socket>();
+        public int N = 30;
+        public int M = 20;
 
-            try
+        public async Task StartAsync()
+        {
+            udpListener = new UdpClient(UdpPort);
+            tcpListener = new TcpListener(IPAddress.Any, TcpPort);
+
+            Console.WriteLine($"[SERVER] UDP listening on {UdpPort}");
+            Console.WriteLine($"[SERVER] TCP listening on {TcpPort}");
+
+            tcpListener.Start();
+            
+            Sektor[,,] sektori = makeSectorMap(N, M);
+
+            _ = HandleUdpRequestsAsync(sektori);
+
+            while (true)
             {
-                //Inicijalizacija sektorske mape
-                Sektor[,,] sektori = new Sektor[N, M, 3];
-                for (int j = 0; j < M; j++)
-                {
-                    for (int i = 0; i < N; i++)
-                    {
-                        for (int k = 0; k < 3; k++)
-                        {
-                            sektori[i, j, k] = new Sektor();
-                            sektori[i, j, k].axisX = i;
-                            sektori[i, j, k].axisY = j;
-                            sektori[i, j, k].axisZ = k+1;
+               
 
-                            sektori[i, j, k].zauzet = false;
+                var tcpClient = await tcpListener.AcceptTcpClientAsync();
+                _ = HandleTcpFlightAsync(tcpClient, sektori);
+
+                 
+            }
+        }
+
+        private async Task HandleUdpRequestsAsync(Sektor[,,] sektori)
+        {
+            while (true)
+            {
+                var result = await udpListener.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(result.Buffer);
+                Console.WriteLine($"[SERVER] UDP request: {message}");
+
+                string[] parts = message.Split(';');
+                if (parts.Length >= 6 && parts[0] == "REQUEST")
+                {
+                    int startX = int.Parse(parts[1]);
+                    int startY = int.Parse(parts[2]);
+                    int startZ = int.Parse(parts[3]);
+                    int endX = int.Parse(parts[4]);
+                    int endY = int.Parse(parts[5]);
+
+                    string response;
+                    lock (LockObj)
+                    {
+                        bool conflict = false;
+                        foreach (var flight in ActiveFlights)
+                        {
                             
+                            if (startX == flight.axisStartY && startY == flight.axisStartX && startX == flight.axisStartZ)
+                            {
+                                conflict = true;
+                                break;
+                            }
                         }
-                        //Predpostavka da na svakoj visini ima nevreme
-                        if(r.Next(0, 100) < 10)
+
+                        if (conflict)
                         {
-                            sektori[i, j, 0].meteoroloskiUslovi = true;
-                            sektori[i, j, 1].meteoroloskiUslovi = true;
-                            sektori[i, j, 2].meteoroloskiUslovi = true;
+                            startX += 1;
+                            startY += 1;
+                            response = $"CORRECTED;{startX};{startY};{startZ}";
+                        }
+                        else
+                        {
+                            response = $"OK;{startX};{startY};{startZ}";
                         }
                     }
-                }
 
-                while(!kraj)
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    await udpListener.SendAsync(responseBytes, responseBytes.Length, result.RemoteEndPoint);
+                }
+                else
                 {
-
-
-
-                    //Ispis sektorske mape
-                    for (int j = 0; j < M; j++)
-                    {
-                        for (int i = 0; i < N; i++)
-                        {
-                            if (sektori[i, j, 0].meteoroloskiUslovi == true)
-                                Console.Write(" X ");
-                            else
-                                Console.Write("[ ]");
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.Read();
+                    string response = "DENIED";
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    await udpListener.SendAsync(responseBytes, responseBytes.Length, result.RemoteEndPoint);
                 }
             }
-            catch(SocketException ex)
+        }
+
+        private async Task HandleTcpFlightAsync(TcpClient tcpClient, Sektor[,,] sektori)
+        {
+            Console.WriteLine("[SERVER] New TCP client connected.");
+
+            using (NetworkStream stream = tcpClient.GetStream())
             {
-                Console.WriteLine($"Doslo je do greske: {ex}");
+                BinaryFormatter formatter = new BinaryFormatter();
+                Let flight = (Let)formatter.Deserialize(stream);
+
+                lock (LockObj)
+                {
+                    ActiveFlights.Add(flight);
+                }
+
+                Console.WriteLine($"[SERVER] Flight added: {flight.letelica.imeLetelice}, " +
+                                  $"Destination: ({flight.axisEndX},{flight.axisEndY})");
+
+                int i = 0;
+                int j = 0;
+                while ((flight.axisStartX != flight.axisEndX) || (flight.axisStartY != flight.axisEndY))
+                {
+                    //kretanje aviona ka destinaciji
+                    if (flight.axisEndX > flight.axisStartX)
+                        flight.axisStartX += 1;
+                    if (flight.axisEndX < flight.axisStartX)
+                        flight.axisStartX -= 1;
+                    if (flight.axisEndY > flight.axisStartY)
+                        flight.axisStartY += 1;
+                    if (flight.axisEndY < flight.axisStartY)
+                        flight.axisStartY -= 1;
+
+                    lock (LockObj)
+                    {
+                        foreach (var other in ActiveFlights)
+                        {
+                            if (other != flight &&
+                                other.axisStartX == flight.axisStartX &&
+                                other.axisStartY == flight.axisStartY &&
+                                other.axisStartZ == flight.axisStartZ)
+                            {
+                                j++;
+                                // Collision
+                                flight.axisStartX += 1;
+                                flight.axisStartY += 1;
+                                Console.WriteLine($"[SERVER] Conflict detected! " +
+                                                  $"Adjusted {flight.letelica.imeLetelice} to " +
+                                                  $"({flight.axisStartX},{flight.axisStartY},{flight.axisStartZ})");
+                                break;
+                            }
+                        }
+                    }
+                                i++;
+
+                    string update = $"Position update {i}: Current locatiion {flight.axisStartX}, {flight.axisStartY}, {flight.axisStartZ} heading to {flight.axisEndX},{flight.axisEndY}\t Number of course corrections: {j}\n";
+                    byte[] buffer = Encoding.UTF8.GetBytes(update);
+                    await stream.WriteAsync(buffer, 0, buffer.Length);
+                    
+                    printSektorMap(sektori, N, M);
+                    await Task.Delay(2000);
+                    
+
+                }
+
+                printSektorMap(sektori, N, M);
+
+                string finalMsg = "ARRIVED at destination.\n";
+                byte[] finalBuffer = Encoding.UTF8.GetBytes(finalMsg);
+                await stream.WriteAsync(finalBuffer, 0, finalBuffer.Length);
+
+                Console.WriteLine("[SERVER] Flight arrived.");
+
+                // Remove flight from active list
+                lock (LockObj)
+                {
+                    ActiveFlights.Remove(flight);
+                }
             }
 
+            tcpClient.Close();
+        }
+        Random r = new Random();
+        public Sektor[,,] makeSectorMap(int N, int M)
+        {
+            Sektor[,,] sektori = new Sektor[N, M, 3];
+            for (int j = 0; j < M; j++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        sektori[i, j, k] = new Sektor();
+                        sektori[i, j, k].axisX = i;
+                        sektori[i, j, k].axisY = j;
+                        sektori[i, j, k].axisZ = k + 1;
 
-            Console.WriteLine("Server zavrsava sa radom");
-            serverSocket.Close();
-            Console.ReadKey();
+                        sektori[i, j, k].zauzet = false;
+
+                    }
+                    //Predpostavka da na svakoj visini ima nevreme
+                    if (r.Next(0, 100) < 10)
+                    {
+                        sektori[i, j, 0].meteoroloskiUslovi = true;
+                        sektori[i, j, 1].meteoroloskiUslovi = true;
+                        sektori[i, j, 2].meteoroloskiUslovi = true;
+                    }
+                }
+            }return sektori;
+        }
+        public void printSektorMap(Sektor[,,] sektori, int N, int M)
+        {
+            Console.Clear();
+            for (int j = 0; j < M; j++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (sektori[i, j, 0].meteoroloskiUslovi == true)
+                        Console.Write(" X ");
+                    else
+                        Console.Write("[ ]");
+                }
+                Console.WriteLine();
+            }
+            
+            
         }
     }
 }
+   
